2d263e27a5aa5f644ea93ec502bb7c5a367a826e
Yuriy Nenakhov <ynenakho@e1z3r4p18.42.us.org>
2018-06-28 20:03:46 -0700

    names changer in ScanvTrap.cpp

--- Day03/ex01/ScavTrap.cpp
+++ Day03/ex01/ScavTrap.cpp
@@ -2,7 +2,7 @@
 
 void ScavTrap::challengeNewcomer(std::string const & target) {
   if (this->energyPoints < 25) {
-    std::cout << "Claptrap " << this->name << " doesnt have enougth energy." << std::endl;
+    std::cout << "CL4P-TP " << this->name << " doesnt have enougth energy." << std::endl;
     return;
   }
 
@@ -14,7 +14,7 @@ void ScavTrap::challengeNewcomer(std::string const & target) {
     " Shoot himself",
     " Cook a cake"
   };
-  std::cout << "Claptrap " << this->name << " made " << target
+  std::cout << "CL4P-TP " << this->name << " made " << target
             << attackArray[rand() % 5]
             << "!" << std::endl;
 
@@ -22,12 +22,12 @@ void ScavTrap::challengeNewcomer(std::string const & target) {
 
 void ScavTrap::beRepaired(unsigned int amount) {
   if (this->hitPoints == this->maxHitPoints) {
-    std::cout << "Claptrap " << this->name << " has maximum hitPoints and cant be repaired." << std::endl;
+    std::cout << "CL4P-TP " << this->name << " has maximum hitPoints and cant be repaired." << std::endl;
     return ;
   }
   this->hitPoints = ((this->hitPoints + (int)amount) < this->maxHitPoints) ?
                           this->hitPoints + amount : this->maxHitPoints;
-  std::cout << "Claptrap " << this->name << " was Repaired. Now he have "
+  std::cout << "CL4P-TP " << this->name << " was Repaired. Now he have "
                         << this->hitPoints << " left." << std::endl;
   return;
 }
@@ -35,12 +35,12 @@ void ScavTrap::beRepaired(unsigned int amount) {
 
 void ScavTrap::rangedAttack(std::string const & target) {
   if (this->energyPoints < 25) {
-    std::cout << "Claptrap " << this->name << " doesnt have enougth energy. "
+    std::cout << "CL4P-TP " << this->name << " doesnt have enougth energy. "
               << this->energyPoints << " points left." << std::endl;
     return ;
   }
   this->energyPoints -= 25;
-  std::cout << "Claptrap " << this->name << " attacks " << target
+  std::cout << "CL4P-TP " << this->name << " attacks " << target
             << " at range, causing " << this->rangedAttackDmg
             << " points of damage!" << std::endl;
   return ;
@@ -53,7 +53,7 @@ void ScavTrap::meleeAttack(std::string const & target) {
     return ;
   }
   this->energyPoints -= 25;
-  std::cout << "Claptrap " << this->name << " attacks " << target
+  std::cout << "CL4P-TP " << this->name << " attacks " << target
             << " in melee, causing " << this->meleeAttackDmg
             << " points of damage!" << std::endl;
   return ;
@@ -62,19 +62,19 @@ void ScavTrap::meleeAttack(std::string const & target) {
 void ScavTrap::takeDamage(unsigned int amount) {
   if ((int)amount < this->hitPoints) {
     this->hitPoints -= amount - this->armorDmgReduction;
-    std::cout << "Claptrap " << this->name << " was damaged on "
+    std::cout << "CL4P-TP " << this->name << " was damaged on "
     << amount - this->armorDmgReduction << " of hit points. Now he have " << this->hitPoints
     << " left." << std::endl;
   }
   else {
     this->hitPoints = 0;
-    std::cout << "Claptrap " << this->name << " was killed." << std::endl;
+    std::cout << "CL4P-TP " << this->name << " was killed." << std::endl;
   }
   return;
 }
 
 ScavTrap::~ScavTrap() {
-  std::cout << "Claptrap named " << this->name << " destructed." << std::endl;
+  std::cout << "CL4P-TP named " << this->name << " destructed." << std::endl;
   return;
 }
 
@@ -88,6 +88,6 @@ ScavTrap::ScavTrap(std::string name) : name(name) {
   this->meleeAttackDmg = 20;
   this->rangedAttackDmg = 13;
   this->armorDmgReduction = 3;
-  std::cout << "Claptrap named " << this->name << " created." << std::endl;
+  std::cout << "CL4P-TP named " << this->name << " created." << std::endl;
   return;
 }
