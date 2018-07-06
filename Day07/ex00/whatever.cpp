047d3fe267bb766db088e394e1b29957a7168654
Yuriy Nenakhov <ynenakho@e1z3r4p18.42.us.org>
2018-07-05 21:57:36 -0700

    Day07 ex00 is done

---
+++ Day07/ex00/whatever.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+template <typename T>
+void swap(T &one, T &two) {
+	T	tmp = one;
+	one = two;
+	two = tmp;
+}
+
+template <typename T>
+T max(T one, T two) {
+	return (one > two ? one : two);
+}
+
+template <typename T>
+T min(T one, T two) {
+	return (one < two ? one : two);
+}
+
+int main() {
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "abcde1";
+	std::string d = "abcde2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return (0);
+}
